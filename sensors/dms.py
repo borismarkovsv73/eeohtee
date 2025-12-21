120ad7eeeaeb955b36b512650b1c191a2dd8090a
borismarkovsv73 <markovboris3@gmail.com>
2025-12-22 00:02:40 +0100

    [fix]: debounce logic to DMS and DS1 sensor classes

--- sensors/dms.py
+++ sensors/dms.py
@@ -13,9 +13,11 @@ class DMS(object):
         ['*', '0', '#', 'D']
     ]
     
-    def __init__(self, row_pins, col_pins):
+    def __init__(self, row_pins, col_pins, bouncetime=0.2):
         self.row_pins = row_pins
         self.col_pins = col_pins
+        self.bouncetime = bouncetime
+        self.last_key_time = {}
         
         GPIO.setmode(GPIO.BCM)
         
@@ -27,13 +29,22 @@ class DMS(object):
             GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
     
     def get_key(self):
+        current_time = time.time()
+        
         for row_idx, row_pin in enumerate(self.row_pins):
             GPIO.output(row_pin, GPIO.LOW)
+            time.sleep(0.001)
             
             for col_idx, col_pin in enumerate(self.col_pins):
                 if GPIO.input(col_pin) == GPIO.LOW:
                     key = self.KEYS[row_idx][col_idx]
-                    time.sleep(0.2)
+                    
+                    if key in self.last_key_time:
+                        if current_time - self.last_key_time[key] < self.bouncetime:
+                            GPIO.output(row_pin, GPIO.HIGH)
+                            return None
+
+                    self.last_key_time[key] = current_time
                     GPIO.output(row_pin, GPIO.HIGH)
                     return key
             
--- sensors/ds.py
+++ sensors/ds.py
@@ -1,23 +1,34 @@
+import time
+
 try:
     import RPi.GPIO as GPIO
 except:
     pass
 
 class DS1(object):
-    def __init__(self, pin):
+    def __init__(self, pin, bouncetime=0.1):
         self.pin = pin
+        self.bouncetime = bouncetime
+        self.last_change_time = 0
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
     def is_pressed(self):
         return GPIO.input(self.pin) == GPIO.LOW
+    
+    def should_process(self):
+        current_time = time.time()
+        if current_time - self.last_change_time >= self.bouncetime:
+            self.last_change_time = current_time
+            return True
+        return False
 
 
 def run_ds_loop(ds, delay, callback, stop_event, name, dl_queue, db_queue):
     last_state = False
     while not stop_event.is_set():
         pressed = ds.is_pressed()
-        if pressed != last_state:
+        if pressed != last_state and ds.should_process():
             last_state = pressed
             if pressed:
                 code = "DS_PRESSED"
@@ -28,3 +39,4 @@ def run_ds_loop(ds, delay, callback, stop_event, name, dl_queue, db_queue):
                 dl_queue.put("DOOR_LOCKED")
                 db_queue.put("DOOR_LOCKED")
             callback(pressed, code, name)
+        time.sleep(0.01)
